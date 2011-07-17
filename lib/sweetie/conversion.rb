72a86d5ee3d245b7a7f974cd74fbc198707fe552
Matthias Guenther <matthias.guenther@wikimatze.de>
2011-07-17 17:49:07 +0200

    Insert YARD documentation and made small refactoring

--- lib/sweetie/conversion.rb
+++ lib/sweetie/conversion.rb
@@ -3,9 +3,12 @@ module Sweetie
 
   class Conversion
 
-    @@config = "../_config.yml"
+    @@config = "_config.yml"
     @@dir    = "_site"
 
+    # Opens the config file and search after the specified parameters.
+    # It saves the gathered information about the build-date, the links,
+    # the images, and the number of html-pages in the jekyll project.
     def self.change_config
       file = File.open(@@config)
       text = ""
@@ -13,7 +16,7 @@ module Sweetie
         if line.match(/build:/)
           text << "build: #{build_time}\n"
         elsif line.match(/htmlpages:/)
-          text << "htmlpages: #{count_html_pages(@@dir)}\n"
+          text << "htmlpages: #{count_all_html_pages(@@dir)}\n"
         elsif line.match(/images:/)
           text << "images: #{count_all_images(@@dir)}\n"
         elsif line.match(/links:/)
@@ -29,60 +32,85 @@ module Sweetie
       end
     end
 
+    # Counts the link of on html page
+    # @param [page] the path of a html page
+    # @return the number of unique links
     def self.count_link_of_one_page(page)
       links = []
-      links = harvest('//a', page, links, 'a')
+      links = harvest('//a', page, links)
       output_count(links)
     end
 
+    # Count the images of one html page
+    # @param (see #self.count_link_of_one_page)
+    # @return the number of unique images
     def self.count_images_of_one_page(page)
       images = []
-      images = harvest('//img', page, images, 'img')
+      images = harvest('//img', page, images)
       output_count(images)
     end
 
-    def self.count_html_pages(dir)
+    # Counts all html pages
+    # @param [Dir] the path of the directory
+    # @return [Fixnum] the number of unique html pages
+    def self.count_all_html_pages(dir)
       pages = []
-      traverse('//html', pages, 'html', dir)
+      traverse('//html', pages, dir)
       output_count(pages)
     end
 
+    # Counts all the links of all html pages
+    # @param (see #self.count_all_html_pages)
+    # @return (see #self.count_all_html_pages)
     def self.count_all_links(dir)
       links = []
-      traverse('//a', links, 'a', dir)
+      traverse('//a', links, dir)
       output_count(links)
     end
 
+    # Counts all the images of all html pages
+    # @param (see #self.count_all_html_pages)
+    # @return (see #self.count_all_html_pages)
     def self.count_all_images(dir)
       images = []
-      traverse('//img', images, 'img', dir)
+      traverse('//img', images, dir)
       output_count(images)
     end
 
+    # Create the actual build time
+    # @return [String] in the date format mm-dd-yyyy
     def self.build_time
       time = Time.now
       "#{time.month}-#{time.day}-#{time.year}"
     end
 
-    def self.traverse(pattern, ar, type, dir)
+    # Traverse the jekyll directory and get the information about a specific
+    # @param [Pattern] is a string for nokogiri after which the html pages should be parsed
+    # @param [Ar] is an empty Array which is used by the harvest method
+    # @param [Dir] the directory in which the html files are stored
+    def self.traverse(pattern, ar, dir)
       Dir.glob(dir+"/**/*") do |file|
         next if file == '.' or file == '..' or file.include?("html~")
         if file.match(/(.*).html/)
-          harvest(pattern, file, ar, type)
+          harvest(pattern, file, ar)
         end
       end
     end
 
-    def self.harvest(pattern, html, ar, type)
+    # Traverse each html page and gather information about the specified html element
+    # @param [Pattern] important for nokogiri
+    # @param [Html] the path for the html file
+    # @param [Ar] and array which stores all the findings produces by nokogiri
+    def self.harvest(pattern, html, ar)
       file = File.open(html)
       doc = Nokogiri::HTML(file)
       doc.xpath(pattern).each do |node|
-        if type == "a"
+        if pattern == "//a"
           ar << node.text
-        elsif type == "img" and ar.include?(node.to_s)
-        elsif type == "img"
+        elsif pattern == "//img" and ar.include?(node.to_s)
+        elsif pattern == "//img"
           ar << node.to_s
-        elsif type == "html"
+        elsif pattern == "//html"
           ar << node
         else
         end
@@ -90,9 +118,10 @@ module Sweetie
       ar
     end
 
-    def self.output_count(file)
-      file.uniq.count
+    # Count the elements
+    # @param [Ar] is an array with all the found html parts
+    def self.output_count(ar)
+      ar.uniq.count # remove duplicates with uniq
     end
   end
-
 end
